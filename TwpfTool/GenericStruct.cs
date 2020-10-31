ea4bb374a27f56e3308ab0b46a3726fdbbf1b358
youarebritish <bishop@projectbc.net>
2020-10-31 14:46:58 -0700

    Minor fix

--- TwpfTool/GenericStruct.cs
+++ TwpfTool/GenericStruct.cs
@@ -7,15 +7,15 @@ namespace TwpfTool
     [DebuggerDisplay("Struct {StructType}")]
     public sealed class GenericStruct
     {
-        public ushort StructType { get; private set; }
+        public uint StructType { get; private set; }
         public readonly IList<Parameter> Parameters = new List<Parameter>();
 
         public static GenericStruct Read(BinaryReader reader)
         {
             var instance = new GenericStruct();
 
-            var parameterCount = reader.ReadUInt16();
-            instance.StructType = reader.ReadUInt16();
+            instance.StructType = reader.ReadUInt32();
+            var parameterCount = instance.StructType & 0xFFFF;
 
             var paramOffsets = new uint[parameterCount];
             for (var i = 0; i < parameterCount; i++)
